394cdbc4b6c327f8d2a02efb6cad1d90845e9f09
Chinwei Vic Hu <vic@cvhu.org>
2015-08-03 22:34:46 -0500

    Add jsoup and http request + html parser

--- src/main/java/com/dediced/beaver/WebLink.java
+++ src/main/java/com/dediced/beaver/WebLink.java
@@ -1,9 +1,21 @@
 package com.dediced.beaver;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 /**
  * Created by cvhu on 7/28/15.
  */
 public class WebLink {
+    public final static String USER_AGENT = "Mozilla/5.0";
 
     private final long linkId;
     private final String url;
@@ -25,6 +37,48 @@ public class WebLink {
 
     public String getContent() {
         return content;
+    }
+
+    public String getHttpContent() {
+
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            // optional default is GET
+            con.setRequestMethod("GET");
+
+            //add request header
+            con.setRequestProperty("User-Agent", USER_AGENT);
+
+            int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'GET' request to URL : " + url);
+            System.out.println("Response Code : " + responseCode);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            //print result
+            return getContentBody(response.toString());
+        } catch (MalformedURLException e) {
+            return "Invalid Url: " + url;
+        } catch (ProtocolException e) {
+            return "Protocol Exception";
+        } catch (IOException e) {
+            return "IOException with response";
+        }
+    }
+
+    public String getContentBody(final String htmlString) {
+            Document doc = Jsoup.parse(htmlString);
+            return doc.body().text();
     }
 
 
--- src/main/java/com/dediced/beaver/WebLinkController.java
+++ src/main/java/com/dediced/beaver/WebLinkController.java
@@ -1,6 +1,7 @@
 package com.dediced.beaver;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebLinkController {
 
     @RequestMapping("/link")
-    public WebLink getTestLink() {
-        return new WebLink(1234l, "url", "content");
+    public WebLink getTestLink(@RequestParam("url") String url) {
+        return new WebLink(1234l, url, "content");
     }
 
 }
